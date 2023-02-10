1ae8244418c2abfda54c18064c298dc30a5c0d83
Alice-321 <124622812+Alice-321@users.noreply.github.com>
2023-02-10 08:03:56 -0800

    Add files via upload
    day1

---
+++ day1/LearningVariables.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class LearningVariables {
+
+	public static void main(String[] args) {
+            float version=100.2f;
+            String carModel="waganor";
+            boolean visible=true;
+            int year=1998;
+            char logo='m';
+            System.out.println("version" + version);
+            System.out.println("car model " + carModel);
+            System.out.println("visible " + visible);
+            System.out.println("Year of manufacturing " + year);
+            System.out.println("logo of the car " + logo);
+            		
+
+	}
+
+}
